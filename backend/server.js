8b6eeff114ba8e4c9f50387f5029daef0795ab5a
Imran Hasan <kevjmbote@gmail.com>
2024-06-02 20:01:47 +0600

    chore: Update server.js to listen on all network interfaces

--- backend/server.js
+++ backend/server.js
@@ -43,6 +43,6 @@ app.use('/api/products', productRoute)
 // Define the PORT
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, ()=>{
-    console.log(`Server listening on port ${PORT}`)
+app.listen(PORT, '0.0.0.0', ()=>{
+    console.log(`Server listening on port${PORT}`)
 })
